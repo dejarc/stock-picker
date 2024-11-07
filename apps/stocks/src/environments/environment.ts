5c3a6ff5793680b1f34f47aadf84ebb3dbc27866
Christopher DeJarlais <cdejarc@gmail.com>
2024-11-07 16:40:36 -0700

    Fix environment files

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,8 +6,8 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: 'https://api.polygon.io',
-  apiURL: ''
+  apiKey: '',
+  apiURL: 'https://api.polygon.io'
 };
 
 /*
